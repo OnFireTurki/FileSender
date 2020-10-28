9b573dad2ad6d13841f0e53840951d7a501284ed
OnFireTurki <52703500+OnFireTurki@users.noreply.github.com>
2020-10-28 22:41:05 +0300

    Add files via upload

---
+++ FileSender/FileSender/Form1.Designer.vb
@@ -0,0 +1,167 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.FileNameBox = New System.Windows.Forms.TextBox()
+        Me.LoadFileButton = New System.Windows.Forms.Button()
+        Me.TokenBox = New System.Windows.Forms.TextBox()
+        Me.IDBox = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'FileNameBox
+        '
+        Me.FileNameBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.FileNameBox.Location = New System.Drawing.Point(12, 12)
+        Me.FileNameBox.Name = "FileNameBox"
+        Me.FileNameBox.Size = New System.Drawing.Size(281, 20)
+        Me.FileNameBox.TabIndex = 0
+        Me.FileNameBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'LoadFileButton
+        '
+        Me.LoadFileButton.AllowDrop = True
+        Me.LoadFileButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.LoadFileButton.Location = New System.Drawing.Point(299, 12)
+        Me.LoadFileButton.Name = "LoadFileButton"
+        Me.LoadFileButton.Size = New System.Drawing.Size(34, 21)
+        Me.LoadFileButton.TabIndex = 1
+        Me.LoadFileButton.Text = "..."
+        Me.LoadFileButton.UseVisualStyleBackColor = True
+        '
+        'TokenBox
+        '
+        Me.TokenBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TokenBox.Location = New System.Drawing.Point(12, 39)
+        Me.TokenBox.Name = "TokenBox"
+        Me.TokenBox.Size = New System.Drawing.Size(321, 20)
+        Me.TokenBox.TabIndex = 2
+        Me.TokenBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'IDBox
+        '
+        Me.IDBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.IDBox.Location = New System.Drawing.Point(12, 65)
+        Me.IDBox.Name = "IDBox"
+        Me.IDBox.Size = New System.Drawing.Size(321, 20)
+        Me.IDBox.TabIndex = 3
+        Me.IDBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 112)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(321, 23)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Send"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(12, 141)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(321, 109)
+        Me.RichTextBox1.TabIndex = 5
+        Me.RichTextBox1.Text = ""
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Checked = True
+        Me.RadioButton1.Location = New System.Drawing.Point(12, 91)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(88, 17)
+        Me.RadioButton1.TabIndex = 6
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Telegram Bot"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(222, 91)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(111, 17)
+        Me.RadioButton2.TabIndex = 7
+        Me.RadioButton2.Text = "Discord Webhook"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(14, 43)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(44, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Token :"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(14, 69)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(24, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "ID :"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(345, 262)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.IDBox)
+        Me.Controls.Add(Me.TokenBox)
+        Me.Controls.Add(Me.LoadFileButton)
+        Me.Controls.Add(Me.FileNameBox)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Form1"
+        Me.Text = "txtFileSender"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents FileNameBox As TextBox
+    Friend WithEvents LoadFileButton As Button
+    Friend WithEvents TokenBox As TextBox
+    Friend WithEvents IDBox As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ FileSender/FileSender/Form1.vb
@@ -0,0 +1,121 @@
+﻿#Region "Imports"
+Imports System.IO
+Imports System.Net
+Imports System.Text
+Imports System.Collections.Specialized
+Imports System.Threading
+#End Region
+#Region "Main-Class"
+Public Class Form1
+    Private Sub LoadFileButton_Click(sender As Object, e As EventArgs) Handles LoadFileButton.Click
+        Using OpenFileDialog As New OpenFileDialog() With {.Title = "Please Select File", .Filter = "txt files (*.txt)|*.txt"}
+            If OpenFileDialog.ShowDialog = DialogResult.OK Then
+                FileNameBox.Text = OpenFileDialog.FileName
+            End If
+        End Using
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        If RadioButton1.Checked Then
+            Label1.Text = "Token :" : Label2.Text = "ID :"
+        End If
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        If RadioButton2.Checked Then
+            Label1.Text = "Webhook :" : Label2.Text = "Content :"
+        End If
+    End Sub
+    ''' <summary>
+    ''' Send Http post request to send txt file and return the response body.
+    ''' </summary>
+    ''' <param name="Url">The Url that we will send the request to it.</param>
+    ''' <param name="FormFields">The Form-Data for request body.</param>
+    ''' <returns>The Value of response body.</returns>
+    Public Function SendPost(Url As String, FormFields As NameValueCollection) As String
+        Try
+            Dim request As HttpWebRequest = CType(HttpWebRequest.Create(Url), HttpWebRequest)
+            Dim boundary As String = "----------------------------" & DateTime.Now.Ticks.ToString("x")
+            request.ContentType = "multipart/form-data; boundary=" & boundary
+            request.Method = "POST"
+            request.KeepAlive = True
+            Dim meStream As MemoryStream = CreateMultipartData(boundary, FileNameBox.Text, FormFields)
+            request.ContentLength = meStream.Length
+            Using requestStream As Stream = request.GetRequestStream()
+                meStream.Position = 0
+                Dim tempBuffer As Byte() = New Byte(meStream.Length - 1) {}
+                meStream.Read(tempBuffer, 0, tempBuffer.Length)
+                meStream.Close()
+                requestStream.Write(tempBuffer, 0, tempBuffer.Length)
+            End Using
+            'get response from the server and return the body.
+            Using response As WebResponse = request.GetResponse()
+                Dim reader2 As New StreamReader(response.GetResponseStream())
+                Return reader2.ReadToEnd()
+            End Using
+        Catch : End Try
+        Return Nothing
+    End Function
+
+    ''' <summary>
+    ''' Create multipart/form-data and return it.
+    ''' </summary>
+    ''' <param name="boundary">boundary.</param>
+    ''' <param name="FilePath">The path of the file we will send.</param>
+    ''' <param name="FormFields">The Form-Data to put it in the multipart/form-data.</param>
+    ''' <returns>The Value of response body.</returns>
+    Public Function CreateMultipartData(boundary As String, FilePath As String, FormFields As NameValueCollection) As MemoryStream
+        Dim FileContent As New FileStream(FilePath, FileMode.Open, FileAccess.Read)
+        Dim meStream As New MemoryStream()
+        Dim Firstboundarybytes As Byte() = Encoding.ASCII.GetBytes(vbCrLf & "--" & boundary & vbCrLf)
+        Dim FinalBoundaryBytes As Byte() = Encoding.ASCII.GetBytes(vbCrLf & "--" & boundary & "--")
+        Dim formdataT As String = vbCrLf & "--" & boundary & vbCrLf & "Content-Disposition: form-data; name=""{0}"";" & vbCrLf & vbCrLf & "{1}"
+        'set the form-data to the multipart/form-data.
+        For Each key As String In FormFields.Keys
+            Dim formitem As String = String.Format(formdataT, key, FormFields(key))
+            Dim formitembytes As Byte() = Encoding.UTF8.GetBytes(formitem)
+            meStream.Write(formitembytes, 0, formitembytes.Length)
+        Next
+        Dim headerTemplate As String = "Content-Disposition: form-data; name={0}; filename=""{1}""" & vbCrLf & "Content-Type: application/octet-stream" & vbCrLf & vbCrLf
+        'After finished up the form now we getting to the hard part the file.
+        meStream.Write(Firstboundarybytes, 0, Firstboundarybytes.Length)
+        'Before we Format the string we must check where the user want send this file; because the parameters name varies from server to server.
+        Dim header As String = String.Format(headerTemplate, If(RadioButton1.Checked, "document", "file1"), FileContent.Name)
+        Dim headerbytes As Byte() = Encoding.UTF8.GetBytes(header)
+        meStream.Write(headerbytes, 0, headerbytes.Length)
+        Dim Bytes As Byte() = New Byte(1023) {}
+        Dim Bytesnum As Integer = FileContent.Read(Bytes, 0, Bytes.Length)
+        While Not Bytesnum = 0
+            meStream.Write(Bytes, 0, Bytesnum)
+            Bytesnum = FileContent.Read(Bytes, 0, Bytes.Length)
+        End While
+        'setup memory stream for request stream.
+        meStream.Write(FinalBoundaryBytes, 0, FinalBoundaryBytes.Length)
+        'Everything is ready now we just need to return the value of meStream.
+        Return meStream
+    End Function
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If String.IsNullOrEmpty(FileNameBox.Text) Then MsgBox("The File is missing!") : Exit Sub
+        Button2.Enabled = False
+        If RadioButton1.Checked AndAlso (String.IsNullOrEmpty(TokenBox.Text) Or String.IsNullOrEmpty(IDBox.Text)) Then
+            MsgBox("Something is missing ! <Bot Token or chat id>")
+        ElseIf RadioButton2.Checked AndAlso String.IsNullOrEmpty(TokenBox.Text) Then
+            MsgBox("Webhook url is Missing!")
+        End If
+        Dim th As New Thread(New ThreadStart(Sub()
+                                                 Dim response As String : Dim url As String : Dim Formdata As New NameValueCollection()
+                                                 url = If(RadioButton1.Checked, String.Format("https://api.telegram.org/bot{0}/SendDocument", TokenBox.Text), TokenBox.Text)
+                                                 'Before we add the form field we must check where the user want send this file; because the parameters name varies from server to server.
+                                                 Formdata.Add(If(RadioButton1.Checked, "chat_id", "Content"), IDBox.Text)
+                                                 Try
+                                                     response = SendPost(url, Formdata)
+                                                 Catch ex As Exception
+                                                     response = "Error " & ex.Message
+                                                 End Try
+                                                 RichTextBox1.Text = $"URL : {url}{vbNewLine}Response : {response}"
+                                                 Button2.Enabled = True
+                                             End Sub)) With {.IsBackground = True} : th.Start()
+    End Sub
+End Class
+#End Region
